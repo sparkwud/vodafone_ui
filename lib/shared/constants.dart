4d759ab96d1a900c8cb9f99b9729b73529e3f6b6
sparkwud <kelvinagbenyo@gmail.com>
2024-01-12 16:20:46 +0000

    add app constants (i.e. strings, colors, icons, images, etc.)

---
+++ lib/shared/constants.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppStrings {
+  static const baseFontFamily = "Satoshi";
+
+  static const usedData = 97.34;
+  static const remainingData = 40.34;
+  static const totalData = 150.00;
+}
+
+class AppColors {
+  static const vodafoneRed = Color(0xFFE30613);
+  static const vodafoneCultured = Color(0xFFF2F8F8);
+  static const dimGrey = Color(0xFF676A75);
+
+  static const cardTitle = Color(0xFF2D2D2D);
+  static const cardSubtitle = Color(0xFF848484);
+}
+
+class AppIcons {
+  // APP BAR ICONS
+  static const vodafoneIconSvg = "assets/icons/ic_vodafone_logo.svg";
+  static const vodafoneIconPng = "assets/icons/ic_vodafone_logo.png";
+  static const notificationIcon = "assets/icons/ic_notifications.png";
+
+  static const cediIcon = "assets/icons/ic_cedi.png";
+  static const wirelessIcon = "assets/icons/ic_wireless.png";
+  static const phoneIcon = "assets/icons/ic_phone.png";
+  static const bookmarkIcon = "assets/icons/ic_bookmark.png";
+  static const rewardIcon = "assets/icons/ic_reward.png";
+  static const simIcon = "assets/icons/ic_sim.png";
+  static const vasIcon = "assets/icons/ic_vas.png";
+  static const plusIcon = "assets/icons/ic_plus.png";
+
+  // BOTTOM NAV BAR ICONS
+  static const servicesIcon = "assets/icons/nav_bar_icons/ic_services.png";
+  static const cashIcon = "assets/icons/nav_bar_icons/ic_cash.png";
+  static const bundlesIcon = "assets/icons/nav_bar_icons/ic_bundles.png";
+  static const settingsIcon = "assets/icons/nav_bar_icons/ic_settings.png";
+}
+
+class AppImages {
+  static const vodafoneTobi = "assets/images/ic_vodafone_tobi.png";
+}
